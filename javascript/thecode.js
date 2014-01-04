922bd3cb2177d5ce87b6c0e349ec007315521c56
Yue Shi <yue.shi@live.com>
2014-01-04 15:08:46 +1100

    format js files

--- javascript/thecode.js
+++ javascript/thecode.js
@@ -1,99 +1,92 @@
 //thecode.js
 $(document).ready(function () {
-  var c = document.getElementById('canvas'),
-      ctx = c.getContext('2d'),
-      lsystem_worker;
-     
-  ctx.imageSmoothingEnabled = false;
+    var c = document.getElementById('canvas'), 
+        ctx = c.getContext('2d'),
+        lsystem_worker; 
 
-  if (typeof(Worker) !== 'undefined') { 
-      lsystem_worker = new Worker('javascript/worker.js'); 
+    ctx.imageSmoothingEnabled = false; 
 
-      lsystem_worker.onmessage = function(e) { 
-          var points = [],
-              offset = {},
-              scale = 0.0;
-    
-          if (e) {
-              points = e.data.points;
-              offset = e.data.offset;
-              scale = e.data.scale;
-
-              if (e.data.drawIteration) {
-                  ctx.restore();
-                  ctx.save();
-                  ctx.translate(-(scale*660-660)/2, -(scale*660-660)/2);
-                  ctx.lineWidth = 1 / scale;
-                  ctx.scale(scale, scale);
-                  ctx.clearRect(0, 0, c.width, c.height);
-                  ctx.beginPath(); 
-                  if (points && points.length !== 0) {
-                      for (var i = 0; i < points.length - 1;) {
-                          drawLine(points[i], points[++i], offset);
-                      }
-                  } 
-
-                  $('.button').prop('disabled', false);
-
-              } else {
-                  ctx.restore();
-                  ctx.clearRect(0, 0, c.width, c.height);
-                  ctx.beginPath(); 
-
-                  if (points.length !== 0) {
-                      var i = 0, th;
-                      th = setInterval(function() {
-                          if (i < points.length - 1) {
-                              drawLine(points[i], points[++i], offset); 
-                          } else {
-                              clearInterval(th);
-                              $('.button').prop('disabled', false);
-                          }
-                      }, 1);
-                  } else {
-                      $('.button').prop('disabled', false);
-                  }
-              } 
-              ctx.closePath(); 
-          }
-      }
-      lsystem_worker.onerror = function(error) {
-          throw error;
-      }
-
-  } else {
-    // show message
-  };
-
-  function roundToHalf(i) { 
-      return Math.round(i * 2) / 2 + (Math.round(i * 2) % 2 === 0 ? 0.5 : 0);
-  }
-
-  function drawLine(start, end, offset) {
-      var startX = roundToHalf(start.x),
-          startY = roundToHalf(start.y),
-          endX = roundToHalf(end.x),
-          endY = roundToHalf(end.y),
-          key, sub, rules, ls; 
-
-      ctx.moveTo(startX + offset.x, startY + offset.y);
-      ctx.lineTo(endX + offset.x, endY + offset.y);
-      ctx.strokeStyle = 'rgb(' + end.color.r + ', ' + end.color.g + ', ' +  end.color.b + ')';
-      ctx.stroke();
-  }
-
-  $('.drawButton, .iterateButton').on('click', function(e) {
-      var ls = buildLS();
+    if (typeof(Worker) !== 'undefined') { 
+        lsystem_worker = new Worker('javascript/worker.js'); 
+        lsystem_worker.onmessage = function(e) { 
+            var points = [],
+                offset = {},
+                scale = 0.0; 
         
-      lsystem_worker.postMessage({
-          'cWidth': c.width,
-          'cHeight': c.height,
-          'drawIteration': $(this).hasClass('iterateButton'),
-          'l_system': ls
-      });    
+            if (e) {
+                points = e.data.points;
+                offset = e.data.offset;
+                scale = e.data.scale; 
+                if (e.data.drawIteration) {
+                    ctx.restore();
+                    ctx.save();
+                    ctx.translate(-(scale*660-660)/2, -(scale*660-660)/2);
+                    ctx.lineWidth = 1 / scale;
+                    ctx.scale(scale, scale);
+                    ctx.clearRect(0, 0, c.width, c.height);
+                    ctx.beginPath(); 
+                    if (points && points.length !== 0) {
+                        for (var i = 0; i < points.length - 1;) { 
+                            drawLine(points[i], points[++i], offset);
+                        }
+                    }
+                    $('.button').prop('disabled', false); 
+                } else {
+                    ctx.restore();
+                    ctx.clearRect(0, 0, c.width, c.height);
+                    ctx.beginPath(); 
 
-      $('.button').prop('disabled', true);
-  });
+                    if (points.length !== 0) {
+                        var i = 0, th;
+                        th = setInterval(function() {
+                            if (i < points.length - 1) {
+                                drawLine(points[i], points[++i], offset); 
+                            } else {
+                                clearInterval(th);
+                                $('.button').prop('disabled', false);
+                            }
+                        }, 1);
+                    } else {
+                        $('.button').prop('disabled', false);
+                    }
+                } 
+                ctx.closePath(); 
+            }
+        }
+        lsystem_worker.onerror = function(error) {
+            throw error;
+        }
+    } else { }; 
+        
+    function roundToHalf(i) { 
+        return Math.round(i * 2) / 2 + (Math.round(i * 2) % 2 === 0 ? 0.5 : 0);
+    } 
+    
+    function drawLine(start, end, offset) {
+        var startX = roundToHalf(start.x),
+            startY = roundToHalf(start.y),
+            endX = roundToHalf(end.x),
+            endY = roundToHalf(end.y),
+            key, sub, rules, ls; 
+
+        ctx.moveTo(startX + offset.x, startY + offset.y);
+        ctx.lineTo(endX + offset.x, endY + offset.y);
+        ctx.strokeStyle = 'rgb(' + end.color.r + ', ' + end.color.g + ', ' +  end.color.b + ')';
+        ctx.stroke();
+    }
+
+    $('.drawButton, .iterateButton').on('click', function(e) {
+        var ls = buildLS();
+        
+        lsystem_worker.postMessage({
+            'cWidth': c.width,
+            'cHeight': c.height,
+            'drawIteration': $(this).hasClass('iterateButton'),
+            'l_system': ls
+        });    
+
+        $('.button').prop('disabled', true);
+    });
 
     function buildLS() {
         var key = $.map($('label.key'), function(i) { return $(i).html()[0].toLowerCase(); }),
--- javascript/worker.js
+++ javascript/worker.js
@@ -3,166 +3,166 @@
 
 var lsystem_worker = function() {
 
-  var RAD = Math.PI / 180,
-      LENGTH = 5;
+    var RAD = Math.PI / 180,
+        LENGTH = 5;
 
-  onmessage = function(e) {
-    var rules = {},
-        seed,
-        iteration,
-        cWidth,
-        cHeight,
-        angle,
-        drawIteration,
-        drawingElements = {};
+    onmessage = function(e) {
+        var rules = {},
+            seed,
+            iteration,
+            cWidth,
+            cHeight,
+            angle,
+            drawIteration,
+            drawingElements = {};
 
-    if (e.data) {
-      cWidth = e.data.cWidth;
-      cHeight = e.data.cHeight;
-      seed = e.data.l_system.axoim;
-      iteration = e.data.l_system.iteration;
-      drawIteration = e.data.drawIteration;
-      angle = e.data.l_system.angle * RAD;
-      rules = e.data.l_system.rules;
+        if (e.data) {
+            cWidth = e.data.cWidth;
+            cHeight = e.data.cHeight;
+            seed = e.data.l_system.axoim;
+            iteration = e.data.l_system.iteration;
+            drawIteration = e.data.drawIteration;
+            angle = e.data.l_system.angle * RAD;
+            rules = e.data.l_system.rules;
 
-      if (drawIteration)
-      {
-          var i = 0;
-          (function delayed (j) {
-              setTimeout(function () {
-                  var s = generateString(seed, ++j, rules);
-                  drawingElements = generateDrawingElements(s, cWidth, cHeight, angle, drawIteration);
-                  drawingElements["drawIteration"] = drawIteration;
-                  postMessage(drawingElements);
-                  if (j < iteration) { 
-                      delayed(j);
-                  }
-              }, 2000);
-          })(i);
-      } else {
-           var s = generateString(seed, iteration, rules);
-           drawingElements = generateDrawingElements(s, cWidth, cHeight, angle, drawIteration);
-		   drawingElements["drawIteration"] = drawIteration;
-           postMessage(drawingElements);
-      }
-    }
-  };
-
-  function Point(x, y, c) {
-    this.x = x;
-    this.y = y;
-    this.color = c;
-  };
-
-  function Color(r, g, b, o) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.opacity = o;
-  };
-
-  function State(c, a) {
-    this.point = c;
-    this.angle = a;
-  };
-
-  function replace(seed, rules) {
-    var newSeed = [];
-    for (var i = 0; i < seed.length; ++i) {
-      if (rules[seed[i]])
-        newSeed = newSeed.concat(rules[seed[i]]);
-      else
-        newSeed.push(seed[i]);
-    }
-    return newSeed;
-  }
-
-  function generateString(seed, iteration, rules) {
-    for (var i = 0; i < iteration; ++i)
-      seed = replace(seed, rules);
-    return seed;
-  }
-
-  function generateDrawingElements(seed, cWidth, cHeight, angle, drawIteration) {
-    var center = new Point(cWidth / 2, cHeight / 2),
-        r = g = b = o = 128,
-        color = new Color(r, g, b, o),
-        start = new Point(center.x, center.y, color),
-        end = new Point(center.x, center.y, color),
-        offset = new Point(0, 0),
-        stack = [],
-        points = [start],
-        right = left = cWidth / 2,
-        top = bottom = cHeight / 2,
-        changeDirection = false,
-        currentAngle = -1.57079633,
-        scale,
-        drawingElements;
-
-    for (var i = 0; i < seed.length; ++i) {
-      switch (seed[i]) {
-        case "f":
-          end.x = start.x + LENGTH * Math.cos(currentAngle);
-          end.y = start.y + LENGTH * Math.sin(currentAngle);
-          points.push(new Point(end.x, end.y, color));
-
-          // redefine boundaries
-          if (start.x < left) left = start.x;
-          else if (start.x > right) right = start.x;
-
-          if (start.y < top) top = start.y;
-          else if (start.y > bottom) bottom = start.y;
-
-          start.x = end.x;
-          start.y = end.y;
-
-          break;
-        case "x":
-          break;
-        case "+":
-          currentAngle -= angle;
-          break;
-        case "-":
-          currentAngle += angle;
-          break;
-        case "[":
-          var c = new Point(start.x, start.y);
-          stack.push(new State(c, currentAngle));
-          break;
-        case "]":
-          var s = stack.pop();
-          start = s.point;
-          currentAngle = s.angle;
-        default:
-          break;
-      }
-    }
-    offset.x = Math.floor(center.x - (left + right) / 2);
-    offset.y = Math.floor(center.y - (top + bottom) / 2);
-
-    imageHeight = bottom - top;
-    imageWidth = right -left;
-
-    dominant = imageHeight - imageWidth > 0 ? imageHeight : imageWidth;
-
-    scale = 600 / dominant;
-
-    if (drawIteration)
-    {
-
-    }
-
-    drawingElements = {
-        points: points,
-        offset: offset,
-        scale: scale
+            if (drawIteration) {
+                var i = 0;
+                (function delayed (j) {
+                    setTimeout(function () {
+                        var s = generateString(seed, ++j, rules);
+                        drawingElements = generateDrawingElements(s, cWidth, cHeight, angle, drawIteration);
+                        drawingElements["drawIteration"] = drawIteration;
+                        postMessage(drawingElements);
+                        if (j < iteration) { 
+                            delayed(j);
+                        }
+                    }, 2000);
+                })(i);
+            } else {
+                var s = generateString(seed, iteration, rules);
+                drawingElements = generateDrawingElements(s, cWidth, cHeight, angle, drawIteration);
+                drawingElements["drawIteration"] = drawIteration;
+                postMessage(drawingElements);
+            }
+        }
     };
 
-    return drawingElements;
-  }
+    function Point(x, y, c) {
+        this.x = x;
+        this.y = y;
+        this.color = c;
+    };
 
-  return {
-    onmessage: onmessage
-  }
+    function Color(r, g, b, o) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.opacity = o;
+    };
+
+    function State(c, a) {
+        this.point = c;
+        this.angle = a;
+    };
+
+    function replace(seed, rules) {
+        var newSeed = [];
+        for (var i = 0; i < seed.length; ++i) {
+          if (rules[seed[i]])
+            newSeed = newSeed.concat(rules[seed[i]]);
+          else
+            newSeed.push(seed[i]);
+        }
+        return newSeed;
+    }
+
+    function generateString(seed, iteration, rules) {
+        for (var i = 0; i < iteration; ++i)
+          seed = replace(seed, rules);
+        return seed;
+    }
+
+    function generateDrawingElements(seed, cWidth, cHeight, angle, drawIteration) {
+        var center = new Point(cWidth / 2, cHeight / 2),
+            r = g = b = o = 128,
+            color = new Color(r, g, b, o),
+            start = new Point(center.x, center.y, color),
+            end = new Point(center.x, center.y, color),
+            offset = new Point(0, 0),
+            stack = [],
+            points = [start],
+            right = left = cWidth / 2,
+            top = bottom = cHeight / 2,
+            changeDirection = false,
+            currentAngle = -1.57079633,
+            scale,
+            drawingElements;
+
+        for (var i = 0; i < seed.length; ++i) {
+            switch (seed[i]) {
+                case "f":
+                    end.x = start.x + LENGTH * Math.cos(currentAngle);
+                    end.y = start.y + LENGTH * Math.sin(currentAngle);
+                    points.push(new Point(end.x, end.y, color));
+
+                      // redefine boundaries
+                    if (start.x < left) left = start.x;
+                    else if (start.x > right) right = start.x;
+
+                    if (start.y < top) top = start.y;
+                    else if (start.y > bottom) bottom = start.y;
+
+                    start.x = end.x;
+                    start.y = end.y;
+
+                    break;
+                case "x":
+                    break;
+                case "+":
+                    currentAngle -= angle;
+                    break;
+                case "-":
+                    currentAngle += angle;
+                    break;
+                case "[":
+                    var c = new Point(start.x, start.y);
+                    stack.push(new State(c, currentAngle));
+                    break;
+                case "]":
+                    var s = stack.pop();
+                    start = s.point;
+                    currentAngle = s.angle;
+                default:
+                    break;
+            }
+        }
+
+        offset.x = Math.floor(center.x - (left + right) / 2);
+        offset.y = Math.floor(center.y - (top + bottom) / 2);
+
+        imageHeight = bottom - top;
+        imageWidth = right -left;
+
+        dominant = imageHeight - imageWidth > 0 ? imageHeight : imageWidth;
+
+        scale = 600 / dominant;
+
+        if (drawIteration)
+        {
+
+        }
+
+        drawingElements = {
+            points: points,
+            offset: offset,
+            scale: scale
+        };
+
+        return drawingElements;
+    }
+
+    return {
+      onmessage: onmessage
+    }
 
 }();
